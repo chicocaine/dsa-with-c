cd4c5ed2ec46b696e82b120047b8fc6613c2b0ff
chicocaine <eduardanthony1208@gmail.com>
2025-09-28 20:31:56 +0800

    fixed random number gen

--- src/util/util.c
+++ src/util/util.c
@@ -1,17 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 
 int get_rand_num(int min, int max) {
-    srand(time(NULL));
-
     int num = min + rand() % (max - min + 1);
     return num;
 }
 
 void fill_int_arr_rand(int *arr, size_t size, int min, int max) {
-    srand(time(NULL));
-
     for (size_t i = 0; i < size; i++) {
         arr[i] = min + rand() % (max - min + 1);
     }
